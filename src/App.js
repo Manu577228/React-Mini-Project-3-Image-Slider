eb15c2c3027ab5d66dc48c47ae19b1f6bd82b311
Manu Bharadwaj BN <102406248+Manu577228@users.noreply.github.com>
2023-11-21 10:23:09 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import ImageSlider from "./components/ImageSlider";
+const App = () => {
+  const slides = [
+    { url: "http://localhost:3000/image-1.jpg", title: "nature-1" },
+    { url: "http://localhost:3000/image-2.jpg", title: "nature-2" },
+    { url: "http://localhost:3000/image-3.jpg", title: "nature-3" },
+    { url: "http://localhost:3000/image-4.jpg", title: "nature-4" },
+    { url: "http://localhost:3000/image-5.jpg", title: "nature-5" },
+    { url: "http://localhost:3000/image-6.jpg", title: "nature-6" },
+  ];
+  const containerStyles = {
+    width: "500px",
+    height: "280px",
+    margin: "0 auto",
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Bharadwaj Image Slider</h1>
+      <div style={containerStyles}>
+        <ImageSlider slides={slides} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
